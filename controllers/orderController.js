227ff2df75020359a8366317fccde6b170dc55ef
Manohar Kumar G <manoharkumarg05@gmail.com>
2024-10-11 14:24:19 +0530

    first commit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,15 +5,13 @@ exports.createOrder = async(req,res,next) =>{
     const cartItems = req.body;
     const amount = Number(cartItems.reduce((preValue,item) => (preValue + item.product.price * item.qty),0)).toFixed(2);
     const status = 'pending';
-    const order = await orderModel.create({cartItems, amount, status});
-    
+    const order = await orderModel.create({cartItems, amount, status});  
 
     // cartItems.forEach(async (item) => {
     //     const product = await productModel.find({name : item.product.name});
     //     product.stock=product.stock-item.qty;
     //     await product.save();
     // });
-
 
     res.json({
         message: "order works",
